0fb9e666bab69bd9ac33dfc482052c8fcd5aee50
dansmht <dan12gm@mail.ru>
2020-09-16 21:24:11 +0300

    Add fetching to mocked json and dynamic creation pizza blocks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Redirect, Switch, Route } from 'react-router-dom';
 
 import { Header } from './components';
 import { Cart, Home } from './pages';
 
-function App() {
+const App = () => {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('./db.json')
+      .then(res => res.json())
+      .then(data => {
+        setPizzas(data.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/">
+            <Home items={pizzas} />
+          </Route>
           <Route exact path="/cart" component={Cart} />
-          <Route render={() => <Redirect to="/" />} />
+          <Redirect to="/" />
         </Switch>
       </div>
     </div>
   );
-}
+};
 
 export default App;
