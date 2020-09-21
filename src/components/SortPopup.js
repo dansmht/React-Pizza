801368bf1de6632d7de5a7cf8c27047a665f6eb4
dansmht <dan12gm@mail.ru>
2020-09-21 21:59:53 +0300

    Fix outside sort popup click for firefox

--- src/components/SortPopup.js
+++ src/components/SortPopup.js
@@ -9,7 +9,8 @@ const SortPopup = React.memo(({ items, activeSortType, onClickSortType }) => {
 
   useEffect(() => {
     const handleOutsideClick = (e) => {
-      if (!e.path.includes(sortRef.current)) {
+      const path = e.path || (e.composedPath && e.composedPath());
+      if (!path.includes(sortRef.current)) {
         setIsVisiblePopup(false);
       }
     };
